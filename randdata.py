2c9c1140d0af595e919f0866be788af127d3d209
Anand Basu <anandbasu@Anands-Air.skidmore.edu>
2024-11-14 11:32:19 -0500

    Random data generator

---
+++ randdata.py
@@ -0,0 +1,9 @@
+import random
+
+# characters to insert
+characters = ['Gandalf', 'Frodo', 'Ron Weasley', 'A rock', 'Gushers', 'Noble 6']
+
+# corresponding genre
+genres = ['Fantasy', 'Fantasy', 'Fantasy', 'Object', 'Object', 'Fantasy']
+
+# generate random vals for each characters stats
