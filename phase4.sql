8b1d67a100a0a8a7ee32e40412d10b3b37c549f2
Basu <abasu@cslab.cs.skidmore.edu>
2024-11-21 12:02:22 -0500

    Finished phase4

--- phase4.sql
+++ phase4.sql
@@ -19,9 +19,48 @@ SELECT M.challenger AS strong_challenger, P.strength AS challenger_strength, M.o
 SELECT COUNT(*) AS Master_Chief_Matches FROM Matches WHERE challenger = 'Master Chief' OR opponent = 'Master Chief';
 
 -- Query 5: Query that uses GROUP BY and HAVING 
--- Returns the average intelligence of the players belonging to each user
-SELECT  uname, avg(P.intelligence) AS avg_intelligence
+-- Returns the average intelligence of each users 3 players, where the average is greater than 5
+SELECT  U.uname, avg(P.intelligence) AS avg_intelligence
 FROM Player P, User U 
 WHERE U.player1 = P.pname OR U.player2 = P.pname OR U.player3 = P.pname 
-GROUP BY (U.uname) HAVING U.points >= 200;
+GROUP BY (U.uname) HAVING AVG(P.intelligence) > 5
+;
+
+-- Query 6: Query with sub-query of set operators
+-- Select Users with players that participated in tournament matches
+SELECT uname FROM User WHERE player1 IN (
+    SELECT challenger FROM Matches WHERE match_id IN (
+        SELECT match_id FROM Tournament
+    )
+)
+UNION
+SELECT uname FROM User WHERE player2 IN (
+    SELECT challenger FROM Matches WHERE match_id IN (
+        SELECT match_id FROM Tournament
+    )
+)
+UNION
+SELECT uname FROM User WHERE player3 IN (
+    SELECT challenger FROM Matches WHERE match_id IN (
+        SELECT match_id FROM Tournament
+    )
+)
+UNION
+SELECT uname FROM User WHERE player1 IN (
+    SELECT opponent FROM Matches WHERE match_id IN (
+        SELECT match_id FROM Tournament
+    )
+)
+UNION
+SELECT uname FROM User WHERE player2 IN (
+    SELECT opponent FROM Matches WHERE match_id IN (
+        SELECT match_id FROM Tournament
+    )
+)
+UNION
+SELECT uname FROM User WHERE player3 IN (
+    SELECT opponent FROM Matches WHERE match_id IN (
+        SELECT match_id FROM Tournament
+    )
+) 
 ;
