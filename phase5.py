a25cc19ee5a2396c96fd2fc3904dec8c2dab980b
Basu <abasu@cslab.cs.skidmore.edu>
2024-12-05 15:30:44 -0500

    Started API

---
+++ phase5.py
@@ -0,0 +1,44 @@
+import mariadb
+
+dbName = 'proj_bf'
+
+dbUser = 'abasu'
+
+dbPass = 'pass3607'
+
+query1_str = 'SELECT pname FROM Player WHERE ? > ?'
+
+query2_str = 'SELECT DISTINCT uname FROM User U, Player P WHERE (U.player1 = P.pname OR U.player2 = P.pname OR U.player3 = P.pname) AND P.? > 7'
+
+query3_str = 'SELECT M.challenger AS strong_challenger, P.strength AS challenger_strength, M.opponent, M.time_stamp FROM Player P, Matches M WHERE strength >= 8 AND P.pname = M.challenger'
+
+class EverythingFantasyAPI:
+
+    def __init__(self):
+        print('connecting to db')
+        try:
+            self.conn = mariadb.connect(
+            user = dbUser,
+            password = dbPass,
+            host="cslab.skidmore.edu",
+            port=3306,
+            database=dbName)
+        except mariadb.Error as e:
+            print(f"Error connecting to MariaDB Platform: {e}")
+            exit(1)
+
+        self.query1_cur = self.conn.cursor(prepared=True)
+        self.query2_cur = self.conn.cursor(prepared=True)
+        self.query3_cur = self.conn.cursor(prepared=True)
+        self.query4_cur = self.conn.cursor(prepared=True)
+        self.query5_cur = self.conn.cursor(prepared=True)
+        self.query6_cur = self.conn.cursor(prepared=True)
+
+    def close(self):
+        self.query1_cur.close()
+        self.query2_cur.close()
+        self.query3_cur.close()
+        self.conn.close()
+        print('Database connection is closed')
+
+
