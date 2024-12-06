9fd4bd3709ed401344fc99c523298754c7169cfc
Fung <mmfung@cslab12.cs.skidmore.edu>
2024-12-05 20:14:58 -0500

    hear me out

--- apitesting.py
+++ apitesting.py
@@ -12,23 +12,44 @@ def main():
     for row in result:
         print('pname: ' + str(row[0]))
 
+    print()
+    result = bf_api.run_q1(9)
+    for row in result:
+        print('pname: ' + str(row[0]))
+
     # run query 2 a few times
     print()
     result = bf_api.run_q2(7)
     for row in result:
         print('uName: ' + str(row[0]))
 
-
     print()
+    result = bf_api.run_q2(9)
+    for row in result:
+        print('uName: ' + str(row[0]))
 
+
+    # run query 3 a few times
+    print()
     result = bf_api.run_q3(8)
     for row in result:
         print(f'pname: {row[0]}, strength: {row[1]}')
+
+
+    # run query 4 a few times
     print()
     result = bf_api.run_q4('Barack Obama')
     print('Barack Obama matches: ' + str(result))
+    
+    print()
     result = bf_api.run_q4('Lime')
     print('Lime matches: ' + str(result))
+
+
+
+
+
+
     bf_api.close()
 
 
